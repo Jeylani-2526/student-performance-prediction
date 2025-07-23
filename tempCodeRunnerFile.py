d74e339d3893056f3a3425c72716338ade720108
Abdalla Jeylani <abdallamuhammed07@gmail.com>
2025-07-23 22:06:07 +0300

    1.image added to the notebook 2.prediction script modified

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import joblib
-# #Load the models
-# joblib.dump(svm, "svm_classifier.joblib")
-# joblib.dump(lr_reg, "linear_regression.joblib")
-# joblib.dump(scaler_cls, "scaler_cls.joblib")   # For classification features
-# joblib.dump(scaler_reg, "scaler_reg.joblib")   # For regression features
